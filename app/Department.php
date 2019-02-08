6f644929182eb9ff6bbb773d6ef4e3cf480b717f
ycl <ycl@ycl-MacBook.local>
2019-02-08 17:09:36 +0300

    Employees create works with departments select dropdown

--- app/Department.php
+++ app/Department.php
@@ -3,9 +3,14 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Employee;
 
 class Department extends Model
 {
     protected $fillable = array('name');
+
+    public function employee() {
+        return $this->hasMany('Employee');
+    }
 
 }
